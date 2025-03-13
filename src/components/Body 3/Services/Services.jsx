762dd3e7d6c2996aaedcfaad7e18f54a2dd5e721
Hassan Butt <1103butt@gmail.com>
2025-03-13 15:49:40 +0500

    initial stage

--- src/components/Body 3/Services/Services.jsx
+++ src/components/Body 3/Services/Services.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="h-screen w-full flex flex-col justify-center items-center bg-gray-100 text-center px-4">
       <h1 className="text-5xl font-bold sm:text-9xl text-[#502c73]">Sorry!</h1>
       <h2 className="text-3xl sm:text-5xl font-semibold mt-4 text-gray-800">
-      Page on working
+      Page on working⚠
       </h2>
       <p className="text-lg sm:text-xl text-gray-500 mt-2">
         The page you're looking for doesn't exist.
