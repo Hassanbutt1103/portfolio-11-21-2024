8950b1d5c973decab2d16df678ca72ae1d16d16e
Hassan Butt <1103butt@gmail.com>
2025-03-13 15:47:07 +0500

    Initial commit

--- src/components/Body 3/Services/Services.jsx
+++ src/components/Body 3/Services/Services.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center bg-gray-100 text-center px-4">
-      <h1 className="text-5xl font-bold sm:text-9xl text-indigo-600">Sorry!</h1>
+      <h1 className="text-5xl font-bold sm:text-9xl text-[#502c73]">Sorry!</h1>
       <h2 className="text-3xl sm:text-5xl font-semibold mt-4 text-gray-800">
       Page on working
       </h2>
@@ -12,7 +12,7 @@ const NotFound = () => {
         The page you're looking for doesn't exist.
       </p>
       
-      <Link to="/" className="mt-6 px-6 py-3 bg-indigo-600 text-white text-lg font-medium rounded-lg shadow-md hover:bg-indigo-700 transition">
+      <Link to="/" className="mt-6 px-6 py-3 bg-[#502c73] text-white text-lg font-medium rounded-lg shadow-md hover:bg-[#a68ac6] transition">
         Go Back Home
       </Link>
     </div>
--- src/components/body 2/Card2.jsx
+++ src/components/body 2/Card2.jsx
@@ -8,12 +8,13 @@ import Typography from "@mui/material/Typography";
 import UIUX from "../../assets/ux-design-7224948_640.png";
 import Graphics from "../../assets/woman-8530607_1920.png";
 import Web from "../../assets/core-web-vitals-6685101_1920.png";
+import { NavLink } from 'react-router-dom';
 
 const MediaCard = () => {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center font-sans bg-[#f4e9ff] rounded-2xl text-gray-300 sm:px-24 px-6 py-6 sm:space-x-6 space-y-6 sm:space-y-0 mt-60">
       {/* Card 1 */}
-      <Card
+      <Card 
         sx={{
           maxWidth: 345,
           borderRadius: "20px",
@@ -51,7 +52,9 @@ const MediaCard = () => {
               },
             }}
           >
+            <NavLink to="/services"> 
             Learn More
+            </NavLink>
           </Button>
         </CardActions>
       </Card>
@@ -94,7 +97,9 @@ const MediaCard = () => {
               },
             }}
           >
+            <NavLink to="/services"> 
             Learn More
+            </NavLink>
           </Button>
         </CardActions>
       </Card>
@@ -137,7 +142,9 @@ const MediaCard = () => {
               },
             }}
           >
+            <NavLink to="/services"> 
             Learn More
+            </NavLink>
           </Button>
         </CardActions>
       </Card>
